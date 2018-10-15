d85bd5c64a7722d7fdd1d9553bf2c1b0e84c45bf
Felipe Huici <felipe.huici@neclab.eu>
2018-10-15 11:48:52 +0200

    Initial commit. Compiles and links, not run yet.
    Signed-off-by: Felipe Huici <felipe.huici@neclab.eu>

---
+++ include/sys/socket.h
@@ -0,0 +1,7 @@
+#ifndef UK_LIBUV_SOCKET
+#define UK_LIBUV_SOCKET
+
+#include <netinet/in.h>
+#include_next <sys/socket.h>
+
+#endif
---
+++ include/sys/time.h
@@ -0,0 +1,8 @@
+#ifndef UK_LIBUV_TIME
+#define UK_LIBUV_TIME
+
+#define CLOCK_MONOTONIC 1
+
+#include_next <sys/time.h>
+
+#endif
